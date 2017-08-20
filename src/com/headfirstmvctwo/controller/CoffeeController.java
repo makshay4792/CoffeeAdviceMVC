846a6f4661d2231bcce341f3c10e04b35598c470
Akshay M <m.akshay4792@gmail.com>
2017-08-20 13:04:40 +0530

    Version 3 result.jsp added - to generate dynamic content controller modified to call result.jsp

--- src/com/headfirstmvctwo/controller/CoffeeController.java
+++ src/com/headfirstmvctwo/controller/CoffeeController.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,17 +17,23 @@ public class CoffeeController extends HttpServlet{
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		PrintWriter out=resp.getWriter();
-		out.println("<br>Hello There!");
+		//PrintWriter out=resp.getWriter();
+		//out.println("<br>Hello There!");
 		String choice=req.getParameter("color");
-		out.println("<br>You have selected "+choice+" color!");
+		//out.println("<br>You have selected "+choice+" color!");
 		
 		CoffeeExpertModel model=new CoffeeExpertModel();
 		List<String> advice=model.getAdvice(choice);
-		Iterator<String> iterator=advice.iterator();
+		/*Iterator<String> iterator=advice.iterator();
 		while(iterator.hasNext()){
 			out.println("<br>You should try "+iterator.next());
-		}
+		}*/
+		
+		req.setAttribute("styles", advice);
+		
+		RequestDispatcher view=req.getRequestDispatcher("result.jsp");
+		
+		view.forward(req, resp);
 	}
 
 }
